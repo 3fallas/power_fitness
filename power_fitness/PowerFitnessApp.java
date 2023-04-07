81286fffd0b6221c4e9051ae2e9fbedf744c96db
christ48 <111949820+christ48@users.noreply.github.com>
2023-04-07 13:57:14 -0600

    Add files via upload

--- power_fitness/PowerFitnessApp.java
+++ power_fitness/PowerFitnessApp.java
@@ -1,23 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Types;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.swing.JOptionPane;
 
 public class PowerFitnessApp {
-    private static final String URL = "jdbc:oracle:thin:@localhost:1521:orcl";
-    private static final String USUARIO = "ethan";
-    private static final String CONTRASEÑA = "Canuto0310";
+    private static final String URL = "jdbc:oracle:thin:@localhost:1521:XE";
+    private static final String USUARIO = "system";
+    private static final String CONTRASEÑA = "1234";
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws SQLException, ParseException {
         // Conexion a la base de datos.
         Connection conexion = DriverManager.getConnection(URL, USUARIO, CONTRASEÑA);
         String[] opciones = { "Agregar Usuario", "Editar Usuario", "Eliminar Usuario", "Agregar Membresía",
-                "Editar Membresía", "Eliminar Membresía", "Agregar Factura", "Ver Facturas", "Agregar Rol" };// Opciones para el menu
+                "Editar Membresía", "Eliminar Membresía", "Agregar Factura", "Ver Facturas", "Agregar Rol","Editar Rol","Eliminar Rol","Agregar datos usuario","actualizar datos usuario","ver datosUsuario","eliminar datos usuario" };// Opciones para el menu
                                                                                               // principal.
 
         int seleccion = JOptionPane.showOptionDialog(null, "Bienvenido a Power Fitness. ¿Qué acción desea realizar?",
@@ -52,7 +62,33 @@ public class PowerFitnessApp {
             case 8:
             agregarRol(conexion);
             break;
+            case 9:
+                Editar_rol(conexion);
+                break;
+            case 10:
+                Eliminar_rol(conexion);
+                break;
+            case 11:
+                Agregar_DatosUsuario(conexion);
+                break;
+            case 12:
+                 actualizar_DatosUsuario(conexion);
+              
+                 
+                 break;
+                 
+            case 13:
+                ver_datosUsuario(conexion);
+                break;
+            case 14:
+               eliminar_DatosUsuario(conexion);
+                break;
+            
+            
             default:
+            
+                
+            
                 break;
         }
     }
@@ -117,8 +153,7 @@ public class PowerFitnessApp {
             String idMembresia = JOptionPane.showInputDialog("Ingrese el nuevo ID de membresía:");
             String idDatosUsuario = JOptionPane.showInputDialog("Ingrese el nuevo ID de datos de usuario:");
 
-            CallableStatement cstmt = connection
-                    .prepareCall("{call editar_usuario(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)}");
+            CallableStatement cstmt = connection.prepareCall("{call editar_usuario(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)}");
             cstmt.setString(1, idUsuario);
             cstmt.setString(2, nombre);
             cstmt.setString(3, primerApellido);
@@ -301,11 +336,11 @@ public class PowerFitnessApp {
         String id = JOptionPane.showInputDialog("Ingrese el ID del rol:");
         String nombre = JOptionPane.showInputDialog("Ingrese el nombre del rol:");
         String descripcion = JOptionPane.showInputDialog("Ingrese la descripción del rol:");
-        
+        String sql="{ call  AGREGAR_ROL(?,?,?)}";
         try {
             // Crear la sentencia SQL para insertar un nuevo registro en la tabla "ROLES"
-            String sql = "INSERT INTO ROLES(ID, NOMBRE, DESCRIPCION) VALUES (?, ?, ?)";
-            PreparedStatement statement = conexion.prepareStatement(sql);
+            
+           CallableStatement statement= conexion.prepareCall(sql);
             
             // Establecer los valores de los parámetros de la sentencia SQL
             statement.setInt(1, Integer.parseInt(id));
@@ -325,5 +360,211 @@ public class PowerFitnessApp {
             JOptionPane.showMessageDialog(null, "Error al agregar el rol: " + e.getMessage());
         }
     }
+public static void ver_rol(Connection conexion){
+
+  try {
+       
+
+        // Crear una consulta SQL
+        String sql = "SELECT ID, nombre, descripcion FROM Roles";
+
+        // Crear un objeto PreparedStatement para ejecutar la consulta
+        PreparedStatement stmt = conexion.prepareStatement(sql);
+
+        // Ejecutar la consulta y obtener los resultados
+        ResultSet rs = stmt.executeQuery();
+
+        // Mostrar los resultados
+        while (rs.next()) {
+            int rollId = rs.getInt("Id");
+            String nombre = rs.getString("nombre");
+            String descripcion = rs.getString("descripcion");
+            JOptionPane.showMessageDialog(null,"Rol #"+rollId+"\n"+"Nombre: "+nombre+"\n"+ "Obligacion:"+descripcion);
+        }
+
+        // Cerrar recursos
+        rs.close();
+        stmt.close();
+    } catch (SQLException e) {
+        e.printStackTrace();
+    } finally {
+        try {
+            if (conexion!= null) {
+                conexion.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
+public static void Editar_rol(Connection conexion){
+           String idString = JOptionPane.showInputDialog(null, "Ingrese el ID de la membresía:", "Editar membresía",
+                JOptionPane.QUESTION_MESSAGE);
+        int id = Integer.parseInt(idString);
+        String nombre= JOptionPane.showInputDialog(null,"ingrese el nuevo nombre:");
+        String descripcion= JOptionPane.showInputDialog(null,"ingrese la nueva descripcion:");
+        try {
+            CallableStatement cr= conexion.prepareCall("{call EDITAR_ROL(?,?,?)}");
+            cr.setInt(1, id);
+            cr.setString(2,nombre);
+            cr.setString(3,descripcion);
+            cr.execute();
+            cr.close();
+            JOptionPane.showMessageDialog(null, "La membresía ha sido editada exitosamente.", "Editar membresía",
+                JOptionPane.INFORMATION_MESSAGE);
+        } catch (SQLException ex) {
+            Logger.getLogger(PowerFitnessApp.class.getName()).log(Level.SEVERE, null, ex);
+            JOptionPane.showMessageDialog(null, "Error .", "Error",
+                JOptionPane.INFORMATION_MESSAGE);
+        }
+        
+
+}
+
+public static void Eliminar_rol(Connection conexion){
+    CallableStatement cr = null;
+    try {
+        String idRolint = JOptionPane.showInputDialog(null, "Ingrese el id del rol a eliminar:");
+        int IdRol = Integer.parseInt(idRolint);
+        cr = conexion.prepareCall("{call eliminar_rol(?)}");
+        cr.setInt(1, IdRol);
+        cr.execute();
+        JOptionPane.showMessageDialog(null,"Se eliminio con ");
+    } catch (SQLException ex) {
+        JOptionPane.showMessageDialog(null, "Error al eliminar la membresía.");
+        ex.printStackTrace();
+    } finally {
+        if (cr != null) {
+            try {
+                cr.close();
+            } catch (SQLException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+}
+
+public static void Agregar_DatosUsuario(Connection conexion) throws SQLException {
+  int id= Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese la id del dato:"));
+  float peso= Float.parseFloat(JOptionPane.showInputDialog(null,"ingrese el peso"));
+  float altura= Float.parseFloat(JOptionPane.showInputDialog(null,"ingrese la altura;"));
+  String fechaIngresada = JOptionPane.showInputDialog(null, "Ingrese la fecha (dd/MM/yyyy)");
+ SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+ java.util.Date fechaUtil;
+
+try {
+    fechaUtil = dateFormat.parse(fechaIngresada);
+} catch (ParseException e) {
+    e.printStackTrace();
+    return;
+}
+java.sql.Date fecha = new java.sql.Date(fechaUtil.getTime());
+  int idusuario =Integer.parseInt(JOptionPane.showInputDialog(null,"ingrese el id de usuario"));
+  String sql="{call llenar_datos_usuario(?,?,?,?,?)}";
+ try{
+ CallableStatement statement= conexion.prepareCall(sql);
+ statement.setInt(1, id);
+ statement.setDouble(2,peso);
+ statement.setDouble(3,altura);
+ statement.setDate(4,fecha);
+ statement.setInt(5,idusuario);
+ statement.execute();
+     System.out.println("Los datos del usuario se agregaron correctamente.");
+} catch (SQLException e) {
+    System.out.println("Error al agregar los datos del usuario: " + e.getMessage());
+}
+ 
+ 
+ }
+public static void ver_datosUsuario(Connection conexion) throws SQLException{
+
+ try{   
+    String sql ="Select ID,PESO,ALTURA,FECHA,ID_USUARIO from  datos_usuario";
+
+ PreparedStatement stmt = conexion.prepareStatement(sql);
+
+        // Ejecutar la consulta y obtener los resultados
+        ResultSet rs = stmt.executeQuery();
+
+        // Mostrar los resultados
+        while(rs.next()){
+                int datoId=rs.getInt("ID");
+                double PesoDATo=rs.getDouble("PESO");
+                double AlturaDATo=rs.getDouble("ALTURA");
+                Date fecha=rs.getDate("FECHA");
+                int IdUsuario=rs.getInt("ID_USUARIO");
+                 JOptionPane.showMessageDialog(null,"ID #"+datoId+"\n"+"Peso: "+PesoDATo+"\n"+ "Altura:"+AlturaDATo+"\n"+"Fecha:"+
+                         fecha+"\n"+"Id usuario"+IdUsuario);
+        }
+        rs.close();
+        stmt.close();
+        } catch (SQLException e) {
+        e.printStackTrace();
+    } finally {
+        try {
+            if (conexion!= null) {
+                conexion.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+
+
+
+public static void actualizar_DatosUsuario(Connection conexion) throws SQLException {
+    int id_usuario = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el ID del usuario a actualizar:"));
+    double peso = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese el nuevo peso:"));
+    double altura = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingrese la nueva altura:"));
+    String fechaIngresada = JOptionPane.showInputDialog(null, "Ingrese la nueva fecha (dd/MM/yyyy):");
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+    java.util.Date fechaUtil = null;
+    try {
+        fechaUtil = dateFormat.parse(fechaIngresada);
+    } catch (ParseException e) {
+        JOptionPane.showMessageDialog(null, "La fecha ingresada no es válida.");
+        return;
+    }
+    java.sql.Date fecha = new java.sql.Date(fechaUtil.getTime());
+    String sql = "{call actualizar_datos_usuario(?,?,?,?)}";
+    try {
+        CallableStatement statement = conexion.prepareCall(sql);
+        statement.setInt(1, id_usuario);
+        statement.setDouble(2, peso);
+        statement.setDouble(3, altura);
+        statement.setDate(4, fecha);
+        statement.execute();
+        JOptionPane.showMessageDialog(null, "Datos de usuario actualizados correctamente.");
+    } catch (SQLException e) {
+        JOptionPane.showMessageDialog(null, "Error al actualizar los datos del usuario: " + e.getMessage());
+    }
+}
+public static void eliminar_DatosUsuario(Connection conexion) throws SQLException {
+    int id_registro = Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese el ID del registro a eliminar:"));
+    String sql = "{call eliminar_datos_usuario(?)}";
+    try {
+        CallableStatement statement = conexion.prepareCall(sql);
+        statement.setInt(1, id_registro);
+        statement.execute();
+        JOptionPane.showMessageDialog(null, "Registro eliminado correctamente.");
+    } catch (SQLException e) {
+        JOptionPane.showMessageDialog(null, "Error al eliminar el registro: " + e.getMessage());
+    }
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
